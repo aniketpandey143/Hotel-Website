6068167434b1def4a1096c4fc3fa8270321bf974
Aniket Pandey <146557181+aniketpandey143@users.noreply.github.com>
2024-11-15 16:05:58 +0530

    Add files via upload

---
+++ HOTEL/JS/main.js
@@ -0,0 +1,27 @@
+// header scroll 
+let nav = document.querySelector(".navbar");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 50){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+} 
+// nav hide 
+ 
+// slide swiper
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3500,
+    },
+});
+
+
+// counter deesign
+// our partner 
